53e22c3286caa569a7f23c6c32c75619c3829e39
Daisuke Murase <typester@cpan.org>
2010-12-10 17:51:11 +0900

    adhoc implementation

---
+++ growl.m
@@ -0,0 +1,227 @@
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#include "ppport.h"
+
+#import <Foundation/Foundation.h>
+#import <Growl.h>
+#import <objc/runtime.h>
+
+static Class GrowlAppBridge;
+
+@interface Growl : NSObject <GrowlApplicationBridgeDelegate> {
+    NSDictionary* info_;
+}
++(Growl*)sharedInstance;
+-(NSDictionary*)info;
+-(void)setInfo:(NSDictionary*)newInfo;
+@end
+
+@implementation Growl
+
++(Growl*)sharedInstance {
+    static Growl* obj = nil;
+    if (nil == obj) {
+        obj = [[Growl alloc] init];
+        [obj setInfo:nil];
+    }
+    return obj;
+}
+
+-(NSDictionary*)info {
+    return info_;
+}
+
+-(void)setInfo:(NSDictionary*)newInfo {
+    if (nil != info_) {
+        [info_ release];
+    }
+    info_ = [newInfo retain];
+}
+
+-(NSDictionary*)registrationDictionaryForGrowl {
+    return info_;
+}
+
+-(void)dealloc {
+    [info_ release];
+    [super dealloc];
+}
+
+@end
+
+XS(growl_installed) {
+    dXSARGS;
+
+    SV* sv;
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    if ([GrowlAppBridge isGrowlInstalled]) {
+        sv = newSViv(1);
+    }
+    else {
+        sv = newSViv(0);
+    }
+    [pool drain];
+
+    ST(0) = sv_2mortal(sv);
+
+    XSRETURN(1);
+}
+
+XS(growl_running) {
+    dXSARGS;
+
+    SV* sv;
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+    if ([GrowlAppBridge isGrowlRunning]) {
+        sv = newSViv(1);
+    }
+    else {
+        sv = newSViv(0);
+    }
+    [pool drain];
+
+    ST(0) = sv_2mortal(sv);
+
+    XSRETURN(1);
+}
+
+XS(growl_register) {
+    dXSARGS;
+
+    if (items < 3) {
+        Perl_croak(aTHX_ "Usage: _growl_register($app_name, \\@allNotifications, \\@defaultNotifications[, $iconFile])");
+    }
+
+    SV* sv_appName = ST(0);
+    SV* sv_allNotif = ST(1);
+    SV* sv_defaultNotif = ST(2);
+
+    if (!SvROK(sv_allNotif) || !SvROK(sv_defaultNotif) ||
+        SVt_PVAV != SvTYPE(SvRV(sv_allNotif)) ||
+        SVt_PVAV != SvTYPE(SvRV(sv_defaultNotif))) {
+
+        Perl_croak(aTHX_ "Error: \\@allNotifications and \\@defaultNotifications should be ArrayRef");
+    }
+
+    STRLEN len;
+    char* ptr;
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+    ptr = SvPV(sv_appName, len);
+    NSString* appName = [NSString stringWithUTF8String:ptr];
+
+    SV** svp;
+    AV* av;
+    int i, alen;
+
+    NSMutableArray* all = [NSMutableArray array];
+    av     = (AV*)SvRV(sv_allNotif);
+    alen = av_len(av) + 1;
+    for (i = 0; i < alen; ++i) {
+        svp = av_fetch(av, i, 0);
+        if (svp) {
+            ptr = SvPV(*svp, len);
+            [all addObject:[NSString stringWithUTF8String:ptr]];
+        }
+    }
+
+    NSMutableArray* defaults = [NSMutableArray array];
+    av     = (AV*)SvRV(sv_defaultNotif);
+    alen = av_len(av) + 1;
+    for (i = 0; i < alen; ++i) {
+        svp = av_fetch(av, i, 0);
+        if (svp) {
+            ptr = SvPV(*svp, len);
+            [defaults addObject:[NSString stringWithUTF8String:ptr]];
+        }
+    }
+
+    NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
+                                            [NSNumber numberWithInt:1], GROWL_TICKET_VERSION,
+                                            appName,                    GROWL_APP_NAME,
+                                            all,                        GROWL_NOTIFICATIONS_ALL,
+                                            defaults,                   GROWL_NOTIFICATIONS_DEFAULT,
+                                       nil];
+
+    [[Growl sharedInstance] setInfo:info];
+
+    [pool drain];
+
+    XSRETURN(0);
+}
+
+XS(growl_notify) {
+    dXSARGS;
+
+    if (items < 3) {
+        Perl_croak(aTHX_ "Usage: _growl_notify($title, $description, $name)");
+    }
+
+    SV* sv_title = ST(0);
+    SV* sv_desc  = ST(1);
+    SV* sv_name  = ST(2);
+
+    STRLEN len;
+    char* s;
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+    s = SvPV(sv_title, len);
+    NSString* title = [NSString stringWithUTF8String:s];
+
+    s = SvPV(sv_desc, len);
+    NSString* description = [NSString stringWithUTF8String:s];
+
+    s = SvPV(sv_name, len);
+    NSString* name = [NSString stringWithUTF8String:s];
+
+    [GrowlAppBridge setGrowlDelegate:[Growl sharedInstance]];
+
+    [GrowlAppBridge notifyWithTitle:title
+                        description:description
+                   notificationName:name
+                           iconData:nil
+                           priority:0
+                           isSticky:NO
+                       clickContext:nil];
+
+    [pool drain];
+
+    XSRETURN(0);
+}
+
+XS(boot_Cocoa__Growl) {
+    SV* dir = get_sv("Cocoa::Growl::FRAMEWORK_DIR", FALSE);
+
+    STRLEN len;
+    char* d = SvPV(dir, len);
+
+    NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+    NSBundle* growl = [NSBundle bundleWithPath:[NSString stringWithUTF8String:d]];
+    NSError* err = nil;
+    if (growl && [growl loadAndReturnError:&err]) {
+        //NSLog(@"growl load success");
+        GrowlAppBridge = objc_getClass("GrowlApplicationBridge");
+    }
+    else {
+        if (nil != err) {
+            Perl_croak(aTHX_ "Coudn't load Growl framework: %s\n",
+                [[err localizedDescription] UTF8String]);
+        }
+        else {
+            Perl_croak(aTHX_ "Coudn't initialize Growl bundle\n");
+        }
+    }
+
+    [pool drain];
+
+    newXS("Cocoa::Growl::growl_installed", growl_installed, __FILE__);
+    newXS("Cocoa::Growl::growl_running", growl_running, __FILE__);
+    newXS("Cocoa::Growl::_growl_register", growl_register, __FILE__);
+    newXS("Cocoa::Growl::_growl_notify", growl_notify, __FILE__);
+}
---
+++ lib/Cocoa/Growl.pm
@@ -0,0 +1,92 @@
+package Cocoa::Growl;
+use strict;
+use warnings;
+use parent 'Exporter';
+use XSLoader;
+
+our $VERSION = '0.01';
+
+our @EXPORT_OK   = qw(growl_installed growl_running growl_register growl_notify);
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
+
+use File::ShareDir ();
+use File::Spec;
+
+our $FRAMEWORK_DIR = do {
+    (my $dist = __PACKAGE__) =~ s/::/-/g;
+    File::Spec->catfile(File::ShareDir::dist_dir($dist), 'Growl.framework');
+};
+
+XSLoader::load __PACKAGE__, $VERSION;
+
+sub growl_notify {
+    my %info = @_;
+
+    my $title       = $info{title} || '';
+    my $description = $info{description} || '';
+    my $notifName   = $info{notificationName} || '';
+
+    _growl_notify($title, $description, $notifName);
+}
+
+sub growl_register {
+    my %info = @_;
+
+    my $appName  = $info{appName} || __PACKAGE__;
+    my $all      = $info{allNotifications};
+    my $defaults = $info{defaultNotifications} || $all;
+
+    _growl_register($appName, $all, $defaults);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Cocoa::Growl - Module abstract (<= 44 characters) goes here
+
+=head1 SYNOPSIS
+
+    use Cocoa::Growl ':all';
+    
+    my $installed = growl_installed(); # true if Growl is installed.
+    my $running   = growl_running();   # true if Growl is running.
+    
+    # register notifications
+    growl_register(
+        appName          => 'My growl script',
+        allNotifications => [qw(Notification1 Notification2)],
+    );
+    
+    # show growl notification
+    growl_notify(
+        notificationName => 'Notification1',
+        title            => 'Hello!',
+        description      => 'Growl world!',
+    );
+
+=head1 DESCRIPTION
+
+Stub documentation for this module was created by ExtUtils::ModuleMaker.
+It looks like the author of the extension was negligent enough
+to leave the stub unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+Daisuke Murase <typester@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2009 by KAYAC Inc.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
